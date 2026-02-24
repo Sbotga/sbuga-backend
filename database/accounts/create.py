75f69f68badf0baf6474d6661b868d2c4ad85624
YumYummity <103061664+YumYummity@users.noreply.github.com>
2026-02-24 00:00:08 -0500

    fix: missing display name in create account

--- database/accounts/create.py
+++ database/accounts/create.py
@@ -2,14 +2,15 @@ from database.query import ExecutableQuery
 
 
 def create_account(
-    account_id: int, username: str, salted_password: str
+    account_id: int, display_name: str, username: str, salted_password: str
 ) -> ExecutableQuery:
     return ExecutableQuery(
         """
-        INSERT INTO account (account_id, username, salted_password)
-        VALUES ($1, $2, $3);
+        INSERT INTO account (account_id, display_name, username, salted_password)
+        VALUES ($1, $2, $3, $4);
         """,
         account_id,
+        display_name,
         username,
         salted_password,
     )
